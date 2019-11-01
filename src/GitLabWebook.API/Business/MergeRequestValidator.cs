73ed77ab65a5dad87eb221895e916f370ed039e2
Juliano Pezzini <juliano.pezzini@benner.com.br>
2019-11-01 01:24:11 -0300

    Initial commit, basic validation, label default for PR without labels

---
+++ src/GitLabWebook.API/Business/MergeRequestValidator.cs
@@ -0,0 +1,62 @@
+﻿using Flurl;
+using Flurl.Http;
+using GitLabWebook.API.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitLabWebook.API.Business
+{
+    public class MergeRequestValidator
+    {
+
+        public IConfiguration Configuration { get; set; }
+
+        
+        public void Validate(GitLabMergeRequest mergeRequest) {
+
+            var gitLabUrl = Configuration["GitLabUrl"];
+            var gitLabPrivateToken = Configuration["GitLabPrivateToken"];
+            var labelForNoLabelMR = Configuration["LabelForNoLabelMR"];
+
+            if (string.IsNullOrEmpty(gitLabUrl)) { 
+                Console.WriteLine("No GitLabUrl configured, look at appsettings.json and try again!");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(gitLabPrivateToken))
+            {
+                Console.WriteLine("No GitLabPrivateToken configured, look at appsettings.json and try again!");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(labelForNoLabelMR))
+            {
+                Console.WriteLine("No LabelForNoLabelMR configured, look at appsettings.json and try again!");
+                return;
+            }
+
+            var newLabels = new List<string>();
+            
+            if (mergeRequest.labels.Length == 0)
+            {
+                Console.WriteLine("Merge request with no Label, assigning " + labelForNoLabelMR + " label");
+                newLabels.Add(labelForNoLabelMR);
+            }
+
+            if (newLabels.Count > 0)
+            {
+                var putMRResource = new GitLabMergeRequestPutResource() { labels = newLabels.ToArray() };
+
+                gitLabUrl
+                    .AppendPathSegments("api", "v4")
+                    .AppendPathSegments("projects", mergeRequest.project.id, "merge_requests")
+                    .AppendPathSegment(mergeRequest.object_attributes.iid)
+                    .SetQueryParam("private_token", gitLabPrivateToken)
+                    .PutJsonAsync(putMRResource);
+            }
+        }
+    }
+}
---
+++ src/GitLabWebook.API/Controllers/GitLabWebHookController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GitLabWebook.API.Business;
+using GitLabWebook.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace GitLabWebook.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GitLabWebHookController : ControllerBase
+    {
+        private readonly IConfiguration Configuration;
+
+        public GitLabWebHookController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // POST api/values
+        [HttpPost]
+        [Route("MergeRequest")]
+        public IActionResult MergeRequest(object obj)
+        {
+            
+            var str = obj.ToString();
+
+            var gitLabMergeRequest =
+                JsonConvert.DeserializeObject<GitLabMergeRequest>(str);
+
+            if (gitLabMergeRequest.object_kind == "merge_request")
+            {
+
+                var validator = new MergeRequestValidator();
+                validator.Configuration = Configuration;
+                validator.Validate(gitLabMergeRequest);
+
+                Console.WriteLine("Merge request validated: " + gitLabMergeRequest.object_attributes.title );
+                
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+    }
+}
